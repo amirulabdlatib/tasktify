68f1ccd9cd8903e4e6c177f3cd87d5b59f500192
Amirul <amirulal73@gmail.com>
2026-08-08 17:07:40 +0800

    refactor: moved workspace index from web to workspace controller

---
+++ app/Http/Controllers/WorkspaceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class WorkspaceController extends Controller
+{
+    public function index()
+    {
+        return view('workspace.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\WorkspaceController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -13,9 +14,7 @@ Route::get('/', function () {
     return redirect()->route('workspace');
 });
 
-Route::get('/workspace', function () {
-    return view('workspace');
-})->middleware(['auth', 'verified'])->name('workspace');
+Route::get('/workspace', [WorkspaceController::class, 'index'])->middleware(['auth', 'verified'])->name('workspace');
 
 Route::resource('tasks', TaskController::class)->middleware(['auth'])->except(['index']);
 
